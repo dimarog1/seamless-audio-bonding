1cf3d675b9377a4b2200e662dd8bc8617b54bd89
dimarog1 <dima.rogatnev@yandex.ru>
2024-03-31 23:04:57 +0300

    fix: models location

--- mix.py
+++ mix.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument('--method', default='fade', type=str)
     parser.add_argument('--audios_path', default='audios', type=str)
     parser.add_argument('--save_path', default='smoothed_audio', type=str)
-    parser.add_argument('--model_path', default='models/model.pt', type=str)
+    parser.add_argument('--model_path', default='model.pt', type=str)
 
 
 if __name__ == '__main__':
