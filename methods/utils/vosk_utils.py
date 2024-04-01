f8640e5e791e6b8c3e7c565df9568e7c8ab0f61f
Anton Muravev <ayumuravev@edu.hse.ru>
2024-04-01 21:16:14 +0300

    VOSK v2
    fix: implement TODO in get_neighbors function

--- methods/utils/vosk_utils.py
+++ methods/utils/vosk_utils.py
@@ -2,8 +2,10 @@ from typing import List
 
 from .vosk_api import *
 
+SAMPLE_RATE = 22050
 
-def determine_words(audio_path: str, sr: int):
+
+def determine_words(audio_path: str, sr: int = SAMPLE_RATE):
     rec_wrapper = RecognizerWrapper(MODEL_PATH, sr)
     result = rec_wrapper.get_transcription(audio_path)
     zipped_recognition = list(zip(result['words'], result['time_steps']))
@@ -11,8 +13,24 @@ def determine_words(audio_path: str, sr: int):
     return zipped_recognition
 
 
-def get_neighbors(source_path: str, splices: List[int]):
+def get_neighbors(source_path: str, splice_samples: List[int]):
+    global SAMPLE_RATE
     data, sr = librosa.load(source_path)
+    SAMPLE_RATE = sr
+    ascending_recognition = determine_words(source_path)
 
-    # TODO: neighbors
-    pass
+    error = 5  # %
+    res_list = []
+
+    for sample in splice_samples:
+        for i in range(len(ascending_recognition)):
+            word, span = ascending_recognition[i]
+            l, r = span
+            if abs(1 - l * SAMPLE_RATE / sample) * 100 <= error:
+                res_list.append((int(ascending_recognition[i - 1 if i - 1 >= 0 else 0][1][0] * SAMPLE_RATE),
+                                 int(r * SAMPLE_RATE)))
+                break
+        else:
+            res_list.append((None, None))
+
+    return res_list
