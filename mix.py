1e9d251fdbd355b91a7e35edf6423e5227921165
dimarog1 <dima.rogatnev@yandex.ru>
2024-04-01 18:28:40 +0300

    add: requirements.txt fix: mix.py

--- mix.py
+++ mix.py
@@ -1,5 +1,5 @@
 import argparse
-from methods.hifivc_method import fade_method, convert_method, fade_convert_method, smooth_pitch_convert_method
+from methods.hifivc_method import convert_method, fade_convert_method, smooth_pitch_convert_method
 from methods.fade_methods import linear_word, linear_time, exp_word, exp_time
 
 
@@ -13,21 +13,19 @@ def main():
     
     a = parser.parse_args()
     
-    if a.method == 'fade':
-        fade_method(a.audios_path, a.save_path)
-    elif a.method == 'convert':
+    if a.method == 'convert':
         convert_method(a.audios_path, a.save_path, a.model_path)
     elif a.method == 'fade_convert':
         fade_convert_method(a.audios_path, a.save_path, a.model_path)
     elif a.method == 'smooth_pitch':
         smooth_pitch_convert_method(a.audios_path, a.save_path, a.model_path)
-    elif a.method == 'linear_word':
+    elif a.method == 'linear_word_fade':
         linear_word(a.audios_path, a.save_path)
-    elif a.method == 'linear_time':
+    elif a.method == 'linear_time_fade':
         linear_time(a.audios_path, a.save_path)
-    elif a.method == 'exp_word':
+    elif a.method == 'exp_word_fade':
         exp_word(a.audios_path, a.save_path)
-    elif a.method == 'exp_time':
+    elif a.method == 'exp_time_fade':
         exp_time(a.audios_path, a.save_path)
 
 
